035dd114e56374c858d02e3b4b167c794f221131
mehmeturmac <mehmeturmac@gmail.com>
2022-06-13 08:03:25 +0300

    mongoose added and pages updated

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
+const Post = require("./models/Post");
 
 const app = express();
+
+//CONNECT DB
+mongoose.connect("mongodb://localhost/cleanblog-test-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //TEMPLATE ENGINE
 app.set("view engine", "ejs");
 
 //MIDDLEWARES
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //ROUTES
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const posts = await Post.find({});
+  res.render("index", { posts });
+  // res.render("index");
 });
 
 app.get("/about", (req, res) => {
@@ -22,8 +34,9 @@ app.get("/add_post", (req, res) => {
   res.render("add_post");
 });
 
-app.get("/post", (req, res) => {
-  res.render("post");
+app.post("/posts", async (req, res) => {
+  await Post.create(req.body);
+  res.redirect("/");
 });
 
 const port = 3000;
